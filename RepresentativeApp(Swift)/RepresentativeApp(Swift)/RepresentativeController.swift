f170eaed56f7cf30b50d3787f90ec2c664352a59
EthanHess <ech1988@gmail.com>
2015-11-07 11:44:54 -0800

    sets up controller with JSON serialization

--- RepresentativeApp(Swift)/RepresentativeApp(Swift)/RepresentativeController.swift
+++ RepresentativeApp(Swift)/RepresentativeApp(Swift)/RepresentativeController.swift
@@ -6,10 +6,40 @@
 //  Copyright © 2015 Ethan Hess. All rights reserved.
 //
 
-import UIKit
+import Foundation
 
-class RepresentativeController: NSObject {
+class RepresentativeController {
     
-    
-
+    static func searchRepsByState(state: String, completion: (representatives: [Representative]) -> Void) {
+        
+        let url = NetworkController.searchURLbyState(state)
+        
+        NetworkController.dataAtURL(url) { (resultData) -> Void in
+            guard let representativeData = resultData  else {
+                print("NO DATA RETURNED")
+                completion(representatives: [])
+                return
+            }
+            
+            do {
+                let resultsDictionary = try NSJSONSerialization.JSONObjectWithData(representativeData, options: NSJSONReadingOptions.AllowFragments) as! [String : AnyObject]
+                
+                let representativeDictionaries = resultsDictionary[Representative.kResultsKey] as! [[String : String]]
+                
+                var arrayOfRepresentativeModelObjects: [Representative] = []
+                
+                for dictionary in representativeDictionaries {
+                    let rep = Representative(jsonDictionary: dictionary)
+                    arrayOfRepresentativeModelObjects.append(rep)
+                }
+                
+                completion(representatives: arrayOfRepresentativeModelObjects)
+                
+            } catch {
+                print("Error serializing JSON")
+                completion(representatives: [])
+                return
+            }
+        }
+    }
 }
